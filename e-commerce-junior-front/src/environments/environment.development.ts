5f881587bf4ac025064d77120ec29da1ca3877ef
Junior peralta <peraltajunior@live.com>
2025-03-04 10:14:34 -0500

    Update environment.development.ts

--- e-commerce-junior-front/src/environments/environment.development.ts
+++ e-commerce-junior-front/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'http://localhost:3000',
-    openaiApiKey: 'OPEN_API_KEY'
+    API_URL: 'http://44.200.231.21:3000',
+    openaiApiKey: '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'
 };
